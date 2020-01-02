4fcd9e21fc3689d9670e1b889c9d2c5b9b99e815
aeryen <aeryen@gmail.com>
2020-01-02 17:19:54 -0600

    a lot of update on regression methods

--- Hotel_REGR_Test.py
+++ Hotel_REGR_Test.py
@@ -156,6 +156,115 @@ class Regr1Module1200(Module):
         return result,raw_outputs,outputs,aspect_doc,sentim_doc
 
 #%%
+def pool_combo(output, start, end):
+    avg_pool = output[start:end, :].mean(dim=0)
+    max_pool = output[start:end, :].max(dim=0)[0]
+    x = torch.cat([output[-1,:], max_pool, avg_pool], 0)
+    return x
+
+def sentence_extract_pool(outputs, mask, p_index):
+    "Pool MultiBatchEncoder outputs into one vector [last_hidden, max_pool, avg_pool]."
+    output = outputs[-1]
+    seq_max = output.size(1)
+    doc_start = mask.int().sum(dim=1)
+    
+    batch = []
+    for doci in range(0,output.shape[0]):
+        pi = p_index[doci,:].nonzero(as_tuple=True)[0].int()
+        doc = []
+        for senti in range( len(pi) ):
+            if senti==0:
+                doc.append( pool_combo(output[doci,:,:], doc_start[doci], pi[senti]) )
+            else:
+                doc.append( pool_combo(output[doci,:,:], pi[senti-1]+1, pi[senti]) )
+            
+        batch.append( torch.stack(doc, 0) )
+
+    return batch
+
+
+#%%
+def masked_concat_pool(outputs, mask):
+    "Pool MultiBatchEncoder outputs into one vector [last_hidden, max_pool, avg_pool]."
+    output = outputs[-1]
+    avg_pool = output.masked_fill(mask[:, :, None], 0).mean(dim=1)
+    avg_pool *= output.size(1) / (output.size(1)-mask.type(avg_pool.dtype).sum(dim=1))[:,None]
+    max_pool = output.masked_fill(mask[:,:,None], -float('inf')).max(dim=1)[0]
+    x = torch.cat([output[:,-1], max_pool, avg_pool], 1)
+    return x
+class Regr2Module1200(Module):
+    "Create a linear classifier with pooling."
+    def __init__(self, n_asp:int, n_rat:int, layers:Collection[int], drops:Collection[float]):
+        print("REGR 1 init")
+        print("Num Aspect: "+str(n_asp) )
+        print("Num Rating: "+str(n_rat) )
+        self.n_asp = n_asp
+        self.n_rat = n_rat
+        
+        mod_layers = []
+        mod_layers += bn_drop_lin( 1200, 50, p=0.5, actn=nn.ReLU(inplace=True) )
+        mod_layers += bn_drop_lin( 50, 1, p=0, actn=None )
+        self.overall = nn.Sequential(*mod_layers)
+        
+        mod_layers = []
+        mod_layers += bn_drop_lin( 1201, 50, p=0.5, actn=nn.ReLU(inplace=True) )
+        mod_layers += bn_drop_lin( 50, self.n_asp, p=0, actn=torch.nn.Softmax(dim=1) )
+#         mod_layers += bn_drop_lin( 50, self.n_asp, p=0, actn=torch.nn.Sigmoid() )
+        self.aspect = nn.Sequential(*mod_layers)
+        
+        mod_layers = []
+        mod_layers += bn_drop_lin( 1201, 50, p=0.5, actn=nn.ReLU(inplace=True) )
+        mod_layers += bn_drop_lin( 50, 1, p=0, actn=None )
+        self.sentiment = nn.Sequential(*mod_layers)
+
+    def forward(self, input:Tuple[Tensor,Tensor,Tensor,Tensor])->Tuple[Tensor,Tensor,Tensor]:
+        raw_outputs,outputs,mask,p_index = input
+        
+        doc_batch = masked_concat_pool(outputs, mask)    # use original combo pool
+        overall_dist = self.overall(doc_batch)  # [4, 1]
+
+        batch = sentence_extract_pool(outputs, mask, p_index) # list of [80?, 1200]
+    
+        temp_all = []
+        for doci in range(0, len(batch)):
+            one_doc_overall = overall_dist[doci,None]
+            sn = batch[doci].shape[0]
+            temp_all.append( one_doc_overall.expand(sn,-1) )
+        temp_all = torch.cat( temp_all , dim=0)
+        
+        allsent_emb = torch.cat(batch, dim=0)
+        allsent_emb = torch.cat( [temp_all, allsent_emb], dim=1 )
+
+        aspect_dist = self.aspect(allsent_emb)         # [n_sentence, aspect]
+        sentiment_dist = self.sentiment(allsent_emb)   # [n_sentence, 1]
+        sent_bmm = aspect_dist * sentiment_dist  # [n_sentence, aspect]
+        
+        result = []
+        aspect_doc = []
+        sentim_doc = []
+        cur = 0
+        for doci in range(0, len(batch)):
+            sn = batch[doci].shape[0]
+            doc = torch.sum(sent_bmm[cur:(cur+sn),:], dim=0, keepdim=True) # [1, 6]
+            asp_w_sum = torch.sum(aspect_dist[cur:(cur+sn),:], dim=0, keepdim=True) # [1, 6]
+            doc = doc / asp_w_sum
+            result.append(doc)
+            
+            aspect_doc.append( aspect_dist[cur:(cur+sn), :] )
+            sentim_doc.append( sentiment_dist[cur:(cur+sn)] )
+            
+            cur = cur + sn
+        
+        result = torch.cat( result, dim=0 )
+        
+        overall_dist = (overall_dist + 0.5) * (self.n_rat-1) # normalize from 0 mean to 0-4 range
+        result = result + overall_dist
+        result = torch.cat( [overall_dist, result], dim=1 )
+        
+        return result,raw_outputs,outputs,aspect_doc,sentim_doc
+
+
+#%%
 def get_text_classifier(arch:Callable, vocab_sz:int, vocab, n_class:int, bptt:int=70, max_len:int=20*70, config:dict=None,
                         drop_mult:float=1., lin_ftrs:Collection[int]=None, ps:Collection[float]=None,
                         pad_idx:int=1) -> nn.Module:
@@ -171,7 +280,7 @@ def get_text_classifier(arch:Callable, vocab_sz:int, vocab, n_class:int, bptt:in
     ps = [config.pop('output_p')] + ps
     init = config.pop('init') if 'init' in config else None
     encoder = SentenceEncoder(bptt, max_len, arch(vocab_sz, **config), vocab, pad_idx=pad_idx)
-    cls_layer = Regr1Module1200(n_asp=6, n_rat=5, layers=layers, drops=ps)
+    cls_layer = Regr2Module1200(n_asp=6, n_rat=5, layers=layers, drops=ps)
     model = SequentialRNN(encoder, cls_layer)
     return model if init is None else model.apply(init)
 
